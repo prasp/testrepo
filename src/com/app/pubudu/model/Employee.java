949c3fb981375dc40f69299fa196e745521aba9f
pubudu <somaweerapubudu@gmail.com>
2017-09-24 07:13:53 +0530

    Added getters and setters to the Employee model class

--- src/com/app/pubudu/model/Employee.java
+++ src/com/app/pubudu/model/Employee.java
@@ -2,4 +2,28 @@ package com.app.pubudu.model;
 
 public class Employee {
 
+	private int Employeeidentification;
+	public int getEmployeeidentification() {
+		return Employeeidentification;
+	}
+	public void setEmployeeidentification(int employeeidentification) {
+		Employeeidentification = employeeidentification;
+	}
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	private String EmployeeName;
+	private String Address;
+	
+	
+	
 }
