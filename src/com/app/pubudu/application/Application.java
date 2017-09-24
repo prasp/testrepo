f86ce25085fa7fa92230a78d01dee47ce9793628
pubudu <somaweerapubudu@gmail.com>
2017-09-24 06:30:13 +0530

    Added the sysout

--- src/com/app/pubudu/application/Application.java
+++ src/com/app/pubudu/application/Application.java
@@ -4,6 +4,8 @@ public class Application {
 	
 	public static void main(String args[]){
 		System.out.println("Sample GIT Application");
+		System.out.println("Sample Application developed to understand the Github");
+		
 	}
 
 }
